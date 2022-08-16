10271b58ffebb3bae13b7ed667dd37ac7c32be3e
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-08-16 23:41:36 +0200

    add unit tests to image endpoint test

--- src/tests/appSpec.ts
+++ src/tests/appSpec.ts
@@ -2,11 +2,37 @@ import * as supertest from "supertest";
 import app from "../app";
 
 //create request object
-const request = supertest(app)
+const request = supertest(app);
 
-describe('Test endpoint response', () => {
-    it('gets the api endpoint', async () => {
-        const response = await request.get('/');
-        expect(response.status).toBe(200);
-    }
-)});
+describe("Test image endpoint response", () => {
+  it("Get (Bad Request) error when not adding query string", async () => {
+    const response = await request.get("/image");
+    expect(response.status).toBe(400);
+  }),
+    it("Get (Not Found) error when inputing wrong image name", async () => {
+      const response = await request.get("/image?filename=imag");
+      expect(response.status).toBe(404);
+    }),
+    it("Get (Bad Request) error when inputing non-numeric value for width or height", async () => {
+      const response = await request.get(
+        "/image?filename=image&width=abc&height=200"
+      );
+      expect(response.status).toBe(400);
+    }),
+    it("Get (Bad Request) error when inputing 0 as value for width or height", async () => {
+      const response = await request.get(
+        "/image?filename=image&width=200&height=0"
+      );
+      expect(response.status).toBe(400);
+    }),
+    it("Get 200 response when inputing the right image name", async () => {
+      const response = await request.get("/image?filename=image");
+      expect(response.status).toBe(200);
+    }),
+    it("Get 200 response when inputing the right image name and numeric values for width and height", async () => {
+      const response = await request.get(
+        "/image?filename=image&width=200&height=200"
+      );
+      expect(response.status).toBe(200);
+    });
+});
